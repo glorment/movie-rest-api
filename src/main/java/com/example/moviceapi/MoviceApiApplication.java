89dd95b331aafbf823596927cf2272e703c65d7c
Clement Chan <glorment@gmail.com>
2022-11-10 22:35:59 +0000

    init

---
+++ src/main/java/com/example/moviceapi/MoviceApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.moviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/moviceapi/MoviceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
